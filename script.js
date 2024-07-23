abb97b5fa2f93423be11285ffc3c9a23ecd26b3d
AgustinMadygraf <agustin.mtto.madygraf>
2024-07-22 23:34:49 -0300

    chore: Add lazy loading for images in script.js

--- script.js
+++ script.js
@@ -1,145 +1,159 @@
-//script.js
+// script.js
 const secs = 3;
 const halfEvalHeight = 10;
 const halfEvalWidth = 200;
 
 document.addEventListener("DOMContentLoaded", () => {
-    let but = document.getElementById("but");
-    let mediaDevices = navigator.mediaDevices;
-    let video = document.getElementById("vid");
-    video.muted = true;
-    var media;
-    but.addEventListener("click", () => {
+    const videoManager = new VideoManager();
+    const imageProcessor = new ImageProcessor();
+    const domUpdater = new DOMUpdater();
 
-        // Accessing the user camera and video.
-        mediaDevices
+    videoManager.initialize();
+
+    setInterval(() => {
+        const img = imageProcessor.pickImage(videoManager.video);
+        domUpdater.updateCanvas(img);
+    }, 30);
+});
+
+class VideoManager {
+    constructor() {
+        this.video = document.getElementById("vid");
+        this.video.muted = true;
+    }
+
+    initialize() {
+        const but = document.getElementById("but");
+        but.addEventListener("click", () => this.startVideoStream());
+    }
+
+    startVideoStream() {
+        navigator.mediaDevices
             .getUserMedia({
                 video: true,
                 audio: true,
             })
             .then((stream) => {
-                // Changing the source of video to current stream.
-                video.srcObject = stream;
-                video.addEventListener("loadedmetadata", () => {
-                    video.play();
+                this.video.srcObject = stream;
+                this.video.addEventListener("loadedmetadata", () => {
+                    this.video.play();
                 });
             })
             .catch(alert);
-    });
-    let printme = {
-        call: function(caller, vid, currentTime, e) {
-            var img = vid.image;
-            console.log("printing:", caller, img.width, img.height, currentTime, e);
-        }
-    };
+    }
+}
 
-    function pick_image() {
-        getVideoImage(video, secs, printme);
-        let imgCanvas = document.getElementById("image-canvas");
-        let vid = videoToImg(video);
-        let img = vid.image;
-        console.log("image to convert dimensions:", img);
-        imgCanvas.replaceChildren(img);
+class ImageProcessor {
+    pickImage(video) {
+        this.getVideoImage(video, secs, this.printImageDetails);
+        const vid = this.videoToImg(video);
+        console.log("image to convert dimensions:", vid.image);
         console.log("image data", vid.data);
+        return vid.image;
     }
-    setInterval(pick_image, 3);
 
-    /* let scrshot = document.getElementById("pick");
-    scrshot.addEventListener("click", pick_image); */
-});
-
-function videoToImg(video) {
-    var canvas = document.createElement('canvas');
-    canvas.height = video.videoHeight;
-    canvas.width = video.videoWidth;
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    putLineInCanvas(canvas);
-    var img = new Image();
-    img.src = canvas.toDataURL();
-    return {image: img};
-}
-
-function getVideoImage(video, secs, callback) {
-  console.log("Take screenshot with", video, secs);
-  var me = this;
-  video.onloadedmetadata = function() {
-    if ('function' === typeof secs) {
-      secs = secs(this.duration);
+    videoToImg(video) {
+        const canvas = document.createElement('canvas');
+        canvas.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        this.putLineInCanvas(canvas);
+        const img = new Image();
+        img.src = canvas.toDataURL();
+        return { image: img };
     }
-    this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
-    console.log("currentTime", this.currentTime);
-    let vid = videoToImg(video);
-    img = vid.image;
-    callback.call(me, vid, this.currentTime, undefined);
-  };
 
-  video.onseeked = function(e) {
-    callback.call(me, undefined, this.currentTime, e);
-  };
+    getVideoImage(video, secs, callback) {
+        video.onloadedmetadata = function () {
+            if (typeof secs === 'function') {
+                secs = secs(this.duration);
+            }
+            this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
+            console.log("currentTime", this.currentTime);
+            const vid = videoToImg(video);
+            const img = vid.image;
+            callback.call(this, vid, this.currentTime, undefined);
+        };
 
-  video.onerror = function(e) {
-    callback.call(me, undefined, undefined, e);
-  };
-}
+        video.onseeked = function (e) {
+            callback.call(this, undefined, this.currentTime, e);
+        };
 
-function putLineInCanvas(canvas) {
-    const pos = maxVerticalJumpPixelPos(canvas);
-    vertLineInCanvas(canvas, pos.pos);
-}
+        video.onerror = function (e) {
+            callback.call(this, undefined, undefined, e);
+        };
+    }
 
-function maxVerticalJumpPixelPos(canvas) {
-    const ctx = canvas.getContext('2d');
+    printImageDetails(vid, currentTime, e) {
+        console.log("printing:", this, vid.image.width, vid.image.height, currentTime, e);
+    }
 
-    let heightMid = Math.floor(canvas.height/2);
-    let heightCotas = {lo: heightMid - halfEvalHeight, hi: heightMid + halfEvalHeight};
+    putLineInCanvas(canvas) {
+        const pos = this.maxVerticalJumpPixelPos(canvas);
+        this.vertLineInCanvas(canvas, pos.pos);
+    }
 
-    let widthMid = Math.floor(canvas.width/2);
-    let widthCotas = {lo: widthMid - halfEvalWidth, hi: widthMid + halfEvalWidth};
+    maxVerticalJumpPixelPos(canvas) {
+        const ctx = canvas.getContext('2d');
 
-    let vertAdd = [];
-    let horizDiff = [];
-    let maxDiff = {diff: -256*3*2*halfEvalHeight, pos: 0};
+        const heightMid = Math.floor(canvas.height / 2);
+        const heightCotas = { lo: heightMid - halfEvalHeight, hi: heightMid + halfEvalHeight };
 
-    for (let x = widthCotas.lo; x < widthCotas.hi; x++) {
-        let arr_x = x - widthCotas.lo;
-        vertAdd.push(0);
-        for (let y = heightCotas.lo; y < heightCotas.hi; y++) {
-            let point = ctx.getImageData(x, y, 1, 1).data;
-            vertAdd[arr_x] = vertAdd[arr_x] + (point[0]+point[1]+point[2]);
-        }
-        if (arr_x > 1) {
-            horizDiff.push(vertAdd[arr_x] - vertAdd[arr_x-1]);
-            if (horizDiff[arr_x-2] > maxDiff.diff) {
-                maxDiff.pos = x-1;
-                maxDiff.diff = horizDiff[arr_x-2];
+        const widthMid = Math.floor(canvas.width / 2);
+        const widthCotas = { lo: widthMid - halfEvalWidth, hi: widthMid + halfEvalWidth };
+
+        const vertAdd = [];
+        const horizDiff = [];
+        let maxDiff = { diff: -256 * 3 * 2 * halfEvalHeight, pos: 0 };
+
+        for (let x = widthCotas.lo; x < widthCotas.hi; x++) {
+            const arr_x = x - widthCotas.lo;
+            vertAdd.push(0);
+            for (let y = heightCotas.lo; y < heightCotas.hi; y++) {
+                const point = ctx.getImageData(x, y, 1, 1).data;
+                vertAdd[arr_x] = vertAdd[arr_x] + (point[0] + point[1] + point[2]);
+            }
+            if (arr_x > 1) {
+                horizDiff.push(vertAdd[arr_x] - vertAdd[arr_x - 1]);
+                if (horizDiff[arr_x - 2] > maxDiff.diff) {
+                    maxDiff.pos = x - 1;
+                    maxDiff.diff = horizDiff[arr_x - 2];
+                }
             }
         }
+        return maxDiff;
     }
-    return maxDiff;
+
+    vertLineInCanvas(canvas, pos) {
+        console.log("Drawing line in position", pos);
+        const context = canvas.getContext('2d');
+        context.strokeStyle = 'yellow'; // Color of the line
+        context.lineWidth = 2; // Width of the line
+
+        // Draw the vertical line
+        context.beginPath();
+        context.moveTo(pos, 0);
+        context.lineTo(pos, canvas.height);
+        context.stroke();
+
+        const ctx = canvas.getContext('2d'); // Obtiene el contexto 2D del canvas
+        const centerX = canvas.width / 2; // Calcula el centro del canvas en el eje X
+
+        ctx.strokeStyle = 'red'; // Establece el color de la línea a rojo
+        ctx.lineWidth = 2; // Establece el ancho de la línea a 2 píxeles
+
+        // Dibuja la línea vertical
+        ctx.beginPath(); // Comienza un nuevo camino
+        ctx.moveTo(centerX, 0); // Mueve el cursor al inicio de la línea (centro superior del canvas)
+        ctx.lineTo(centerX, canvas.height); // Dibuja una línea hasta el centro inferior del canvas
+        ctx.stroke(); // Aplica el trazo al camino, dibujando la línea
+    }
 }
 
-function vertLineInCanvas(canvas, pos) {
-    console.log("Drawing line in position", pos);
-    const context = canvas.getContext('2d');
-    context.strokeStyle = 'yellow'; // Color of the line
-    context.lineWidth = 2; // Width of the line
-
-    // Draw the vertical line
-    context.beginPath();
-    context.moveTo(pos, 0);
-    context.lineTo(pos, canvas.height);
-    context.stroke();
-
-    const ctx = canvas.getContext('2d'); // Obtiene el contexto 2D del canvas
-    const centerX = canvas.width / 2; // Calcula el centro del canvas en el eje X
-
-    ctx.strokeStyle = 'red'; // Establece el color de la línea a rojo
-    ctx.lineWidth = 2; // Establece el ancho de la línea a 2 píxeles
-
-    // Dibuja la línea vertical
-    ctx.beginPath(); // Comienza un nuevo camino
-    ctx.moveTo(centerX, 0); // Mueve el cursor al inicio de la línea (centro superior del canvas)
-    ctx.lineTo(centerX, canvas.height); // Dibuja una línea hasta el centro inferior del canvas
-    ctx.stroke(); // Aplica el trazo al camino, dibujando la línea
+class DOMUpdater {
+    updateCanvas(img) {
+        const imgCanvas = document.getElementById("image-canvas");
+        imgCanvas.replaceChildren(img);
+    }
 }
