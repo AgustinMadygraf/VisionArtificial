ed67e559b469bdd94b3c12fb12e0d535c79466c3
AgustinMadygraf <agustin.mtto.madygraf>
2024-07-23 00:33:32 -0300

    chore: Update script.js to import VideoManager class
    The code changes include updating the import statement in script.js to import the VideoManager class from the videoManager.js file. This ensures that the updated functionality of the VideoManager class is used in the script.
    
    Note: The code changes include modifying the script.js file.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // js/script.js
 
-//import VideoManager from './videoManager.js';
+import VideoManager from './videoManager.js';
 
 const secs = 3;
 const halfEvalHeight = 10;
@@ -19,32 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 30);
 });
 
-class VideoManager {
-    constructor() {
-        this.video = document.getElementById("vid");
-        this.video.muted = true;
-    }
-
-    initialize() {
-        const but = document.getElementById("but");
-        but.addEventListener("click", () => this.startVideoStream());
-    }
-
-    startVideoStream() {
-        navigator.mediaDevices
-            .getUserMedia({
-                video: true,
-                audio: true,
-            })
-            .then((stream) => {
-                this.video.srcObject = stream;
-                this.video.addEventListener("loadedmetadata", () => {
-                    this.video.play();
-                });
-            })
-            .catch(alert);
-    }
-}
 
 class ImageProcessor {
     pickImage(video) {
