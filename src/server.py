597e6ae2c5ccfadcc9d49dc8bc3eded995844859
SantiGamer 55 <santiagoluanvega@gmail.com>
2024-07-31 15:58:13 -0300

    Cambio de localhost a ip local - para que pueda enviar informacion de un dispositivo

--- src/server.py
+++ src/server.py
@@ -5,6 +5,7 @@ import asyncio
 import websockets
 from ssl_config import SSLConfig
 from logs.config_logger import logger_configurator
+import json
 
 # Obtener el logger configurado
 logger = logger_configurator.get_logger()
@@ -13,8 +14,19 @@ class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.path = '/index.html'
+        elif self.path == '/local-ip':
+            self.handle_local_ip()
+            return
         logger.info(f"Handling GET request for {self.path}")
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+    def handle_local_ip(self):
+        local_ip = get_local_ip()
+        response = {'ip': local_ip}
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode('utf-8'))
 
 def get_local_ip():
     """Obtiene la dirección IP local de la máquina."""
@@ -56,11 +68,12 @@ def run_server():
     http_thread.start()
 
     # Iniciar el servidor WebSocket
-    start_websocket_server()
+    start_websocket_server(local_ip, ssl_config)
 
-def start_websocket_server():
+def start_websocket_server(local_ip, ssl_config):
     async def main():
-        async with websockets.serve(websocket_handler, "localhost", 8765):
+        ssl_context = ssl_config.get_ssl_context()
+        async with websockets.serve(websocket_handler, local_ip, 8765, ssl=ssl_context):
             await asyncio.Future()  # Run forever
 
     asyncio.run(main())
