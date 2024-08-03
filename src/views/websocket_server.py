76b0843344dec70147225d190315eda957037cee
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-03 12:03:59 -0300

    refactor: Add exception handling and logging to WebSocketServer class

--- src/views/websocket_server.py
+++ src/views/websocket_server.py
@@ -13,20 +13,25 @@ class WebSocketServer:
         self.ssl_config = ssl_config
 
     async def handler(self, websocket, path):
-        async for message in websocket:
-            logger.info(f"Received message: {message}")
-            try:
-                message_as_int = int(message[25:])
-                tolerancia = 10
-                upper_threshold = tolerancia
-                lower_threshold = -tolerancia
+        try:
+            async for message in websocket:
+                logger.info(f"Received message: {message}")
+                try:
+                    message_as_int = int(message[25:])
+                    tolerancia = 10
+                    upper_threshold = tolerancia
+                    lower_threshold = -tolerancia
 
-                if message_as_int > upper_threshold:
-                    self.send_http_request('http://192.168.0.184/ena_f')
-                elif message_as_int < lower_threshold:
-                    self.send_http_request('http://192.168.0.184/ena_r')
-            except ValueError:
-                logger.error("Failed to convert message to integer")
+                    if message_as_int > upper_threshold:
+                        self.send_http_request('http://192.168.0.184/ena_f')
+                    elif message_as_int < lower_threshold:
+                        self.send_http_request('http://192.168.0.184/ena_r')
+                except ValueError:
+                    logger.error("Failed to convert message to integer")
+        except websockets.exceptions.ConnectionClosedError as e:
+            logger.error(f"WebSocket connection closed: {e}")
+        except Exception as e:
+            logger.error(f"Unhandled exception in WebSocket handler: {e}")
 
     def send_http_request(self, url):
         try:
@@ -39,11 +44,12 @@ class WebSocketServer:
     def start(self):
         async def main():
             ssl_context = self.ssl_config.get_ssl_context()
-            server = await websockets.serve(self.handler, self.address[0], 8765, ssl=ssl_context)
-            
-            # Set the reuse address option
-            server.sockets[0].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            
-            await asyncio.Future()  # Run forever
+            try:
+                server = await websockets.serve(self.handler, self.address[0], 8765, ssl=ssl_context)
+                # Set the reuse address option
+                server.sockets[0].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                await asyncio.Future()  # Run forever
+            except Exception as e:
+                logger.error(f"Failed to start WebSocket server: {e}")
 
         asyncio.run(main())
