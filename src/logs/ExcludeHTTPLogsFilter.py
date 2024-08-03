23bfe318b959bc3fd435035019b9ad27fa6e07b8
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-03 15:13:49 -0300

    refactor: Update ExcludeHTTPLogsFilter to filter HTTP logs more efficiently

--- src/logs/ExcludeHTTPLogsFilter.py
+++ src/logs/ExcludeHTTPLogsFilter.py
@@ -3,7 +3,5 @@ import logging
 
 class ExcludeHTTPLogsFilter(logging.Filter):
     def filter(self, record):
-        # Excluir mensajes DEBUG y mensajes que contienen "GET / HTTP/1.1" y similares
-        if record.levelno == logging.DEBUG:
-            return False
+        # Filtrar mensajes que contienen "GET / HTTP/1.1" y similares
         return 'GET /' not in record.getMessage() and 'POST /' not in record.getMessage()
