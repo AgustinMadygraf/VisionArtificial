136b7053ffcc68acd54a0a99fedd5613f339376f
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-03 14:22:00 -0300

    refactor: Update logger configuration and formatting

---
+++ run.py
@@ -0,0 +1,17 @@
+#IOTIMAGEPROC\run.py
+import sys
+import os
+
+# Asegúrate de que el directorio `src` esté en el `PYTHONPATH`
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
+from src.main import run_server
+#from src.models.update_repo import RepoUpdater
+
+if __name__ == '__main__':
+    repo_path = 'C:\AppServ\www\IOTIMAGEPROC'
+    #updater = RepoUpdater(repo_path)
+    #updater.run()
+    web_dir = '.'
+    os.chdir(web_dir)
+    run_server()
