b5d33753cc381dc0639fbd701aa82341f70d1c16
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-04 19:52:30 -0300

    refactor: Update WebSocket import in imageProcessor.js

--- static/js/utils/canvasUtils.js
+++ static/js/utils/canvasUtils.js
@@ -1,14 +1,8 @@
-// js/canvasUtils.js
+// static/js/utils/canvasUtils.js
 
-/**
- * Dibuja una línea vertical en el canvas.
- * @param {CanvasRenderingContext2D} context - El contexto del canvas en el que se va a dibujar.
- * @param {number} pos - La posición horizontal en la que se dibujará la línea.
- * @param {string} [color='yellow'] - El color de la línea.
- */
 export function drawVerticalLine(context, pos, color = 'yellow') {
-    context.strokeStyle = color; // Color de la línea
-    context.lineWidth = 2; // Ancho de la línea
+    context.strokeStyle = color;
+    context.lineWidth = 2;
 
     context.beginPath();
     context.moveTo(pos, 0);
@@ -16,15 +10,9 @@ export function drawVerticalLine(context, pos, color = 'yellow') {
     context.stroke();
 }
 
-/**
- * Dibuja una línea horizontal en el canvas.
- * @param {CanvasRenderingContext2D} context - El contexto del canvas en el que se va a dibujar.
- * @param {number} pos - La posición vertical en la que se dibujará la línea.
- * @param {string} [color='green'] - El color de la línea.
- */
 export function drawHorizontalLine(context, pos, color = 'green') {
-    context.strokeStyle = color; // Color de la línea
-    context.lineWidth = 2; // Ancho de la línea
+    context.strokeStyle = color;
+    context.lineWidth = 2;
 
     context.beginPath();
     context.moveTo(0, pos);
@@ -32,17 +20,10 @@ export function drawHorizontalLine(context, pos, color = 'green') {
     context.stroke();
 }
 
-/**
- * Dibuja una regla horizontal en el centro del canvas.
- * @param {CanvasRenderingContext2D} context - El contexto del canvas en el que se va a dibujar.
- * @param {string} [color='blue'] - El color de las líneas de la regla.
- * @param {number} [lineLength=10] - La longitud de cada línea de la regla.
- * @param {number} [spacing=10] - El espacio entre cada línea de la regla.
- */
 export function drawCenterRuler(context, color = 'blue', lineLength = 10, spacing = 10) {
-    const centerY = context.canvas.height / 2; // Centro vertical del canvas
-    context.strokeStyle = color; // Color de las líneas
-    context.lineWidth = 2; // Ancho de las líneas
+    const centerY = context.canvas.height / 2;
+    context.strokeStyle = color;
+    context.lineWidth = 2;
 
     for (let x = 0; x < context.canvas.width; x += spacing) {
         context.beginPath();
