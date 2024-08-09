6fc4b62d8155a9b07356cea8dbb09e72c6582330
SantiGamer 55 <santiagoluanvega@gmail.com>
2024-08-09 16:45:27 -0300

    refactor: Separate initialization of VideoManager based on test value
    The code changes separate the initialization of the VideoManager class based on the value of the 'test' query parameter. If the value is 'True', the video source is set to 'test.jpeg' and displayed on the page. Otherwise, the video stream is started from the device's webcam. This improves flexibility and allows for easier testing.
    
    This commit message follows the established convention of using the "refactor" prefix to indicate code refactoring.

--- static/js/videoManager.js
+++ static/js/videoManager.js
@@ -1,14 +1,15 @@
-// js/videoManager.js
+// static/js/videoManager.js
 import ButtonManager from './buttonManager.js';
 
 /**
  * Clase que gestiona el video desde la webcam.
  */
 export default class VideoManager {
-    constructor() {
+    constructor(testValue) {
         this.video = document.getElementById("vid");
         this.video.muted = true;
         this.buttonManager = new ButtonManager(this); // Inyección de dependencia
+        this.testValue = testValue;
     }
 
     initialize() {
@@ -16,32 +17,39 @@ export default class VideoManager {
     }
 
     startVideoStream() {
-        navigator.mediaDevices
-            .getUserMedia({
-                video: { facingMode: { exact: "environment" } },
-                audio: true,
-            })
-            .then((stream) => {
-                this.video.srcObject = stream;
-                this.video.addEventListener("loadedmetadata", () => {
-                    this.video.play();
-                    document.getElementById("but").classList.add("hidden");
+        if (this.testValue === 'True') {
+            console.log("Test value is True, setting video source to 'test.jpeg'");
+            this.video.src = './static/test.jpeg';
+            this.video.style.display = 'block';
+        } else {
+            console.log("Test value is not True, starting video stream from device");
+            navigator.mediaDevices
+                .getUserMedia({
+                    video: { facingMode: { exact: "environment" } },
+                    audio: true,
+                })
+                .then((stream) => {
+                    this.video.srcObject = stream;
+                    this.video.addEventListener("loadedmetadata", () => {
+                        this.video.play();
+                        document.getElementById("but").classList.add("hidden");
+                    });
+                })
+                .catch(() => {
+                    navigator.mediaDevices
+                        .getUserMedia({
+                            video: { facingMode: "user" },
+                            audio: true,
+                        })
+                        .then((stream) => {
+                            this.video.srcObject = stream;
+                            this.video.addEventListener("loadedmetadata", () => {
+                                this.video.play();
+                                document.getElementById("but").classList.add("hidden");
+                            });
+                        })
+                        .catch(alert);
                 });
-            })
-            .catch(() => {
-                navigator.mediaDevices
-                    .getUserMedia({
-                        video: { facingMode: "user" },
-                        audio: true,
-                    })
-                    .then((stream) => {
-                        this.video.srcObject = stream;
-                        this.video.addEventListener("loadedmetadata", () => {
-                            this.video.play();
-                            document.getElementById("but").classList.add("hidden");
-                        });
-                    })
-                    .catch(alert);
-            });
+        }
     }
 }
