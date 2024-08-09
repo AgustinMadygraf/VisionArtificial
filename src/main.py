6cb9d4be320a88d5784850c092feca36e878fab8
SantiGamer 55 <santiagoluanvega@gmail.com>
2024-08-09 15:27:25 -0300

    AUTOMATIZADO LA INSTALACION DE LIBRERIAS PIPFILE

--- src/main.py
+++ src/main.py
@@ -23,40 +23,7 @@ async def run_server():
     http_server.start()
 
     websocket_server = WebSocketServer(ssl_service, websocket_handler)
-    await websocket_server.start()  # Await the coroutine
-
-async def run_store_data():
-    while True:
-        print("")
-        print('Storing data...')
-        print("")
-        #os.system('python src/store_data.py')
-        #await asyncio.sleep(300)  # Espera de 300 segundos     
-    
-###################################################################
-#async def run_main():
-#    await asyncio.gather(
-#        run_server(),
-#        run_store_data()
-#    )
-####################################################################
-
-def schedule_task():
-    # Definir los minutos específicos en los que deseas ejecutar la tarea
-    #minutes = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
-    minutes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
-    
-    for minute in minutes:
-        schedule.every().hour.at(f":{minute:02d}").do(lambda: asyncio.run(run_store_data()))
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-async def run_main():
-    # Aquí puedes agregar otras tareas que necesites ejecutar
-    loop = asyncio.get_event_loop()
-    loop.run_in_executor(None, schedule_task)
+    await websocket_server.start()
 
 if __name__ == '__main__':
-    asyncio.run(run_main())
+    asyncio.run(run_server())
