71001dd3ba9f9f3c6595bb77ea26ba81293db0d1
AgustinMadygraf <agustin,mtto.madygraf@gmail.com>
2024-08-15 11:38:59 -0300

    refactor: Update SSL certificate and key paths in network configuration

--- src/logs/config_logger.py
+++ src/logs/config_logger.py
@@ -10,7 +10,7 @@ from abc import ABC, abstractmethod
 from src.logs.info_error_filter import InfoErrorFilter
 
 class ConfigStrategy(ABC):
-    """Abstract base class for configuration strategies."""    
+    """Abstract base class for configuration strategies."""
     @abstractmethod
     def load_config(self):
         """Loads configuration from a specific source."""
@@ -35,12 +35,7 @@ class JSONConfigStrategy(ConfigStrategy):
 
 class LoggerConfigurator:
     """Configures logging for the application using a strategy pattern."""
-    # pylint: disable=too-few-public-methods
-    def __init__(
-        self,
-        config_strategy=None,
-        default_level=logging.INFO
-    ):
+    def __init__(self, config_strategy=None, default_level=logging.INFO):
         self.config_strategy = config_strategy or JSONConfigStrategy()
         self.default_level = default_level
 
@@ -51,11 +46,11 @@ class LoggerConfigurator:
             logging.config.dictConfig(config)
         else:
             logging.basicConfig(level=self.default_level)
+            logging.warning("Logging configuration file not found. Using default settings.")
         return logging.getLogger(__name__)
 
 # Configuración inicial del logger para módulos individuales
 initial_config_strategy = JSONConfigStrategy()
 logger_configurator = LoggerConfigurator(config_strategy=initial_config_strategy)
 logger = logger_configurator.configure()
-#logger.addFilter(ExcludeHTTPLogsFilter())
-logger.addFilter(InfoErrorFilter())
+logger.addFilter(InfoErrorFilter())  # Aplica el filtro InfoErrorFilter
