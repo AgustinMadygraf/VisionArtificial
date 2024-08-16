e5b3af42c9b3048b9fc04dfc138e746af4b38c07
AgustinMadygraf <agustin,mtto.madygraf@gmail.com>
2024-08-16 15:44:22 -0300

    15.44

--- src/services/http_service.py
+++ src/services/http_service.py
@@ -31,7 +31,6 @@ class HTTPService(HTTPInterface):
                 time.sleep(2 ** attempt)  # Exponential backoff before retrying
         self.logger.error(f"Failed to fetch data from {url} after {retries} attempts.")
         return None
-<<<<<<< HEAD
 
     def send_request(self, url):
         """
@@ -46,5 +45,3 @@ class HTTPService(HTTPInterface):
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Failed to connect to {url}: {str(e).split(':', maxsplit=1)[0]}")
             raise e
-=======
->>>>>>> d2f75969804ab16c87b377a1df218c2d6465ad89
