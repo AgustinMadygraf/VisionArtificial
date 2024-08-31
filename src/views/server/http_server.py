920da60b1fd9c33e4ceafd3e61dd2f46efc4c17b
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-31 17:04:10 -0300

    refactor: Update HTTP server start method to handle exceptions

--- src/views/server/http_server.py
+++ src/views/server/http_server.py
@@ -23,19 +23,32 @@ class HTTPServer:
         """
         Start the server.
         """
+        self.logger.debug("Initializing HTTP server with address: %s and handler: %s",
+                          self.address, self.handler_class)
         try:
-            self.logger.debug("Starting HTTP server with address: %s and handler: %s", self.address,
-                              self.handler_class)
+            self.logger.debug("Creating HTTPServer instance")
             httpd = http.server.HTTPServer(self.address, self.handler_class)
+            self.logger.debug("HTTPServer instance created successfully")
+
+            self.logger.debug("Getting SSL context")
             ssl_context = self.ssl_config.get_ssl_context()
+            self.logger.debug("SSL context obtained successfully")
+
+            self.logger.debug("Wrapping socket with SSL context")
             httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
-            self.logger.info("Servidor corriendo en https://%s:%s", self.address[0],
-                             self.address[1])
-            self.logger.info("Modo Test en https://%s:%s?test=True", self.address[0],
-                             self.address[1])
+            self.logger.debug("Socket wrapped with SSL context successfully")
+
+            self.logger.info("Servidor corriendo en https://%s:%s",
+                             self.address[0], self.address[1])
+            self.logger.info("Modo Test en https://%s:%s?test=True",
+                             self.address[0], self.address[1])
+
+            self.logger.debug("Starting HTTP server thread")
             http_thread = threading.Thread(target=httpd.serve_forever)
             http_thread.daemon = True
             http_thread.start()
+            self.logger.debug("HTTP server thread started successfully")
+
         except OSError as e:
             self.logger.error("OSError occurred: %s", e)
             raise
