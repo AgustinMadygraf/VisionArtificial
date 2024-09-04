4f21c80187bac11cae925ec67f5710daf6c7fa70
AgustinMadygraf <agustin,mtto.madygraf@gmail.com>
2024-09-04 17:28:28 -0300

    refactor: Improve video stream initialization and performance

--- static/js/videoStreamManager.js
+++ static/js/videoStreamManager.js
@@ -2,7 +2,6 @@
  static/js/videoStreamManager.js
 Este archivo contiene la clase VideoStreamManager, que se encarga de gestionar el stream de video.
  */
-
 /**
  * Clase que gestiona el stream de video.
  */
@@ -13,9 +12,25 @@ export default class VideoStreamManager {
 
     async startStream() {
         try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-            this.videoElement.srcObject = stream;
-            this.videoElement.play();
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            const videoDevices = devices.filter(device => device.kind === 'videoinput');
+            let constraints = { video: { facingMode: { exact: "environment" } } };
+
+            if (videoDevices.length > 0) {
+                try {
+                    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                    this.videoElement.srcObject = stream;
+                    this.videoElement.play();
+                } catch (error) {
+                    console.warn("No se pudo acceder a la cámara trasera, utilizando la cámara frontal.", error);
+                    constraints = { video: { facingMode: "user" } };
+                    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                    this.videoElement.srcObject = stream;
+                    this.videoElement.play();
+                }
+            } else {
+                console.error("No se encontraron dispositivos de video.");
+            }
         } catch (error) {
             console.error("Error starting video stream:", error);
             throw error;
