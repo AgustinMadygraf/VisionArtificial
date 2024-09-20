42f955d63ce34191a9f46f25f8bb18f7c40d0eb5
SantiGamer 55 <santiagoluanvega@gmail.com>
2024-09-20 16:23:16 -0300

    Refactor: Adjust color filter values in ColorFilter.js and optimize brown edge detection in VerticalLineStrategy.js

--- static/js/imageProcessor/ColorFilter.js
+++ static/js/imageProcessor/ColorFilter.js
@@ -13,25 +13,16 @@ export default class ColorFilter {
         const imageData = ctx.getImageData(0, 0, width, height);
         const data = imageData.data;
 
-        // Ajuste de los valores RGB típicos del marrón y las tolerancias
-        const r_set = 150; // Ajustar valor base de rojo para marrón
-        const g_set = 150;  // Ajustar valor base de verde para marrón
-        const b_set = 150;  // Ajustar valor base de azul para marrón
-        const tolerancia_r = 120; // Aumentar tolerancia para mayor rango de marrones
-        const tolerancia_g = 120;
-        const tolerancia_b = 120;
-
         for (let i = 0; i < data.length; i += 4) {
             const r = data[i];
             const g = data[i + 1];
             const b = data[i + 2];
-
+        // Verificar si el color es amarillo o naranja
+        const isBrown = r > g && g > b ;
+        const isYellow = r > 200 && g > 200 && b < 100;
+        const isNotYellow = !isYellow;
             // Verificar si el color es marrón usando umbrales y relaciones
-            if (
-                r >= r_set - tolerancia_r && r <= r_set + tolerancia_r &&
-                g >= g_set - tolerancia_g && g <= g_set + tolerancia_g &&
-                b >= b_set - tolerancia_b && b <= b_set + tolerancia_b &&
-                r > g && r > b // Condición adicional para tonos marrones
+            if (isBrown && isNotYellow
             ) {
                 // Mantener el color original
                 data[i] = r; 
